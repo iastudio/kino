ad69cfa707b6de0ef3c7e88bc3e92d7c7fd1f4ec
Ivan Rudoy - Work <rudooy@gmail.com>
2014-08-15 16:33:49 +1100

    + modal, forms, buttons

--- dest/js/app.js
+++ dest/js/app.js
@@ -1,6 +1,22 @@
 
 $(function(){
 
+  $.modal.defaults = {
+    overlay: "#000",        // Overlay color
+    opacity: 0.75,          // Overlay opacity
+    zIndex: 1,              // Overlay z-index.
+    escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
+    clickClose: true,       // Allows the user to close the modal by clicking the overlay
+    closeText: '',     // Text content for the close <a> tag.
+    closeClass: '',         // Add additional class(es) to the close <a> tag.
+    showClose: true,        // Shows a (X) icon/link in the top-right corner
+    modalClass: "modal",    // CSS class added to the element being displayed in the modal.
+    spinnerHtml: null,      // HTML appended to the default spinner during AJAX requests.
+    showSpinner: true,      // Enable/disable the default spinner during AJAX requests.
+    fadeDuration: null,     // Number of milliseconds the fade transition takes (null means no transition)
+    fadeDelay: 1.0          // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
+  };
+
   ///////////////////
   //    CALENDAR   //
   ///////////////////
